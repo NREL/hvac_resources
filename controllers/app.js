07c0dd4baad9bc31afbc44e244b0f07d2141f13c
Marjorie Schott <marjorie.schott@nrel.gov>
2016-04-15 14:15:10 -0600

    chiller icon menu option
    testing out an icon menu for subsections with tool tips

--- controllers/app.js
+++ controllers/app.js
@@ -125,3 +125,9 @@ $scope.hoverOut = function(){
     this.hoverEdit = false;
 };*/
 /* animation */
+
+// Select all elements with data-toggle="tooltips" in the document
+$('[data-toggle="tooltip"]').tooltip(); 
+
+// Select a specified element
+$('#myTooltip').tooltip();
