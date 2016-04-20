71433a8b1510f2d95c0179ddba275274c80bee8f
Alex Swindler <Alex.Swindler@nrel.gov>
2016-04-20 13:11:56 -0600

    Added nested routing

--- controllers/app.js
+++ controllers/app.js
@@ -1,132 +1,111 @@
-var routerApp = angular.module('routerApp', ['ui.router', 'ngAnimate']);
+var routerApp = angular.module('routerApp', ['ngAnimate', 'ui.router', 'ui.router.stateHelper']);
 
-routerApp.config(function ($stateProvider, $urlRouterProvider) {
+routerApp.config(function (stateHelperProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/home');
 
-  $stateProvider
-
-    // HOME STATES AND NESTED VIEWS ========================================
-    .state('home', {
+  stateHelperProvider
+    .state({
+      name: 'home',
       url: '/home',
       templateUrl: 'views/home.html'
     })
-
-    // Space Loads NESTED VIEWS ========================================
-    .state('space_loads', {
+    .state({
+      name: 'central_plant',
+      url: '/central_plant',
+      templateUrl: 'views/central_plant.html'
+    })
+    .state({
+      name: 'boiler',
+      url: '/boiler',
+      templateUrl: 'views/boiler.html',
+      children: [{
+        name: 'design',
+        url: '/design',
+        templateUrl: 'views/boiler_design.html'
+      }, {
+        name: 'basics',
+        url: '/basics',
+        templateUrl: 'views/boiler_basics.html'
+      }]
+    })
+    .state({
+      name: 'chiller',
+      url: '/chiller',
+      templateUrl: 'views/chiller.html',
+      children: [{
+        name: 'types',
+        url: '/types',
+        templateUrl: 'views/chiller_types.html'
+      }, {
+        name: 'design',
+        url: '/chiller_design',
+        templateUrl: 'views/chiller_design.html'
+      }, {
+        name: 'basics',
+        url: '/basics',
+        templateUrl: 'views/chiller_basics.html'
+      }, {
+        name: 'codes',
+        url: '/codes',
+        templateUrl: 'views/chiller_codes.html'
+      }, {
+        name: 'tools',
+        url: '/tools',
+        templateUrl: 'views/chiller_tools.html'
+      }, {
+        name: 'procure',
+        url: '/procure',
+        templateUrl: 'views/chiller_procure.html'
+      }, {
+        name: 'cases',
+        url: '/cases',
+        templateUrl: 'views/chiller_cases.html'
+      }, {
+        name: 'operation',
+        url: '/operation',
+        templateUrl: 'views/chiller_operation.html'
+      }]
+    })
+    .state({
+      name: 'tower',
+      url: '/tower',
+      templateUrl: 'views/tower.html',
+      children: [{
+        name: 'basics',
+        url: '/basics',
+        templateUrl: 'views/tower_basics.html'
+      }]
+    })
+    .state({
+      name: 'pump',
+      url: '/pump',
+      templateUrl: 'views/pump.html',
+      children: [{
+        name: 'design',
+        url: '/design',
+        templateUrl: 'views/pump_design.html'
+      }, {
+        name: 'basics',
+        url: '/basics',
+        templateUrl: 'views/pump_basics.html'
+      }]
+    })
+    .state({
+      name: 'distribution_system',
+      url: '/distribution_system',
+      templateUrl: 'views/distribution_system.html'
+    })
+    .state({
+      name: 'space_loads',
       url: '/space_loads',
       templateUrl: 'views/space_loads.html'
     })
 
-    // Central Plant NESTED VIEWS ========================================
-    .state('central_plant', {
-      url: '/central_plant',
-      templateUrl: 'views/central_plant.html'
-    })
-
-    // Boiler STATES AND NESTED VIEWS ========================================
-    .state('boiler', {
-      url: '/boiler',
-      templateUrl: 'views/boiler.html'
-    })
-
-    .state('boiler.design', {
-      url: '/design',
-      templateUrl: 'views/boiler_design.html'
-    })
-
-    .state('boiler.basics', {
-      url: '/basics',
-      templateUrl: 'views/boiler_basics.html'
-    })
-
-    // Chiller STATES AND NESTED VIEWS ========================================
-    .state('chiller', {
-      url: '/chiller',
-      templateUrl: 'views/chiller.html'
-    })
-
-    // nested list with custom controller
-    .state('chiller.types', {
-      url: '/types',
-      templateUrl: 'views/chiller_types.html'
-    })
-
-    // nested list with just some random string data
-    .state('chiller.design', {
-      url: '/chiller_design',
-      templateUrl: 'views/chiller_design.html'
-    })
-
-    // nested list with just some random string data
-    .state('chiller.basics', {
-      url: '/basics',
-      templateUrl: 'views/chiller_basics.html'
-    })
-
-    // nested list with just some random string data
-    .state('chiller.codes', {
-      url: '/chiller_codes',
-      templateUrl: 'views/chiller_codes.html'
-    })
-    
-      // nested list with just some random string data
-    .state('chiller.tools', {
-      url: '/chiller_tools',
-      templateUrl: 'views/chiller_tools.html'
-    })
-  
-        // nested list with just some random string data
-    .state('chiller.procure', {
-      url: '/chiller_procure',
-      templateUrl: 'views/chiller_procure.html'
-    })
-  
-          // nested list with just some random string data
-    .state('chiller.cases', {
-      url: '/chiller_cases',
-      templateUrl: 'views/chiller_cases.html'
-     }) 
-  
-          // nested list with just some random string data
-    .state('chiller.operation', {
-      url: '/chiller_operation',
-      templateUrl: 'views/chiller_operation.html'
-     })   
-  
-      // Cooling Tower STATES AND NESTED VIEWS ========================================
-    .state('tower', {
-      url: '/tower',
-      templateUrl: 'views/tower.html'
-    })
-      .state('tower.basics', {
-      url: '/basics',
-      templateUrl: 'views/tower_basics.html'
-    })
-
-    // Pump STATES AND NESTED VIEWS ========================================
-    .state('pump', {
-      url: '/pump',
-      templateUrl: 'views/pump.html'
-    })
-
-    // nested list with custom controller
-    .state('pump.design', {
-      url: '/design',
-      templateUrl: 'views/pump_design.html'
-    })
-
-    // nested list with just some random string data
-    .state('pump.basics', {
-      url: '/basics',
-      templateUrl: 'views/pump_basics.html'
-    })
-
-
 }); // closes $routerApp.config()
 
 
-$(function() {
+$(function () {
   var $body = $('body');
   $body.on('mouseenter', '#diagram a', function (evt) {
     var id = $(evt.target).attr('id');
@@ -140,50 +119,50 @@ $(function() {
 
 //not sure what this is for
 /*
-routerApp.controller('leftnavController', function ($scope) {
+ routerApp.controller('leftnavController', function ($scope) {
 
-  $scope.state = false;
+ $scope.state = false;
 
-  $scope.toggleState = function () {
-    $scope.state = !$scope.state;
-  };
+ $scope.toggleState = function () {
+ $scope.state = !$scope.state;
+ };
 
-});
+ });
 
-routerApp.directive('sidebarDirective', function () {
-  return {
-    link: function (scope, element, attr) {
-      scope.$watch(attr.sidebarDirective, function (newVal) {
-        if (newVal) {
-          element.addClass('show');
-          return;
-        }
-        element.removeClass('show');
-      });
-    }
-  };
-});
-*/
+ routerApp.directive('sidebarDirective', function () {
+ return {
+ link: function (scope, element, attr) {
+ scope.$watch(attr.sidebarDirective, function (newVal) {
+ if (newVal) {
+ element.addClass('show');
+ return;
+ }
+ element.removeClass('show');
+ });
+ }
+ };
+ });
+ */
 
 //Hider
 /*
-function MyCtrl($scope) {
-  $scope.visible = fase;
+ function MyCtrl($scope) {
+ $scope.visible = fase;
 
-  $scope.toggle = function() {
-    $scope.visible = !$scope.hidden;
-  };
-}
-*/
+ $scope.toggle = function() {
+ $scope.visible = !$scope.hidden;
+ };
+ }
+ */
 
 /*
-$scope.hoverIn = function() {
-  this.hoverEdit = true;
-};
+ $scope.hoverIn = function() {
+ this.hoverEdit = true;
+ };
 
-$scope.hoverOut = function() {
-  this.hoverEdit = false;
-};*/
+ $scope.hoverOut = function() {
+ this.hoverEdit = false;
+ };*/
 /* animation */
 
 
